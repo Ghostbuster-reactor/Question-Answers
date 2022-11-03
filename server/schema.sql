7dadcf2a87fb5cbba252f2f55cb0b34f036c7a2c
Tessa Thornberry <tthornberryclass@gmail.com>
2022-11-03 13:53:27 -0700

    successfully added sql file for loading schema rather than using async functions

--- server/schema.sql
+++ server/schema.sql
@@ -1,33 +1,39 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE qanda;
 
-USE chat;
+\c qanda;
+
+-- //can include compression type
+CREATE TABLE IF NOT EXISTS questions (id SERIAL PRIMARY KEY, product_id INTEGER, body VARCHAR(255), date_written INTEGER, asker_name VARCHAR, asker_email VARCHAR(50), reported BOOLEAN, helpful INTEGER);
 
 
+CREATE TABLE IF NOT EXISTS answers (id SERIAL PRIMARY KEY, question_id INTEGER, body VARCHAR(255), date_written INTEGER, answerer_name VARCHAR, answerer_email VARCHAR(50), reported BOOLEAN, helpful INTEGER, FOREIGN KEY (question_id) REFERENCES questions(id));
 
 
-CREATE TABLE users (
-  id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL
-  /* Describe your table here.*/
-);
+CREATE TABLE IF NOT EXISTS answers_photos (id SERIAL PRIMARY KEY, answer_id INTEGER, url VARCHAR, FOREIGN KEY (answer_id) REFERENCES answers (id));
 
--- CREATE TABLE rooms (
+-- CREATE TABLE users (
 --   id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
---   roomname VARCHAR(255) NOT NULL
---   -- message INTEGER(11),
---   -- FOREIGN KEY (message) REFERENCES messages(id)
+--   username VARCHAR(255) NOT NULL
 --   /* Describe your table here.*/
 -- );
 
-CREATE TABLE messages (
-  id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-  text VARCHAR(255) NOT NULL,
-  userid INTEGER(11),
-  roomname VARCHAR(255)
-  /* Describe your table here.*/
-);
+-- -- CREATE TABLE rooms (
+-- --   id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
+-- --   roomname VARCHAR(255) NOT NULL
+-- --   -- message INTEGER(11),
+-- --   -- FOREIGN KEY (message) REFERENCES messages(id)
+-- --   /* Describe your table here.*/
+-- -- );
 
-/* Create other tables and define schemas for them here! */
+-- CREATE TABLE messages (
+--   id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
+--   text VARCHAR(255) NOT NULL,
+--   userid INTEGER(11),
+--   roomname VARCHAR(255)
+--   /* Describe your table here.*/
+-- );
+
+-- /* Create other tables and define schemas for them here! */
 
 
 /*  Execute this file from the command line by typing:
