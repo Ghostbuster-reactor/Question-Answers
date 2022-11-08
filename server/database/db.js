c95df7d3ad1ee122c61d441ac00908593e62e974
Tessa Thornberry <tthornberryclass@gmail.com>
2022-11-08 15:52:11 -0800

    cleaned up database code

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const { Pool } = require('pg');
 const Promise = require("bluebird");
-// console.log('name', process.env.DB_NAME)
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -13,27 +12,9 @@ const pool = new Pool({
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 20000, //this was 2000
 });
-//make username?
+
 
 const db = Promise.promisifyAll(pool, { multiArgs: true });
-
-// db.getAllQuestions = // db.connectAsync() // this connects and creates the tables
-//   .then(() => console.log(`Connected to PostGresql in database, on ${process.env.PORT}`))
-//   .catch((err) => console.log(err));
-
-  //might need this later to manage clients
-// pool.connect((err, client, release) => {
-//   if (err) {
-//     return console.error('error in database', err.stack)
-//   }
-//   client.query('SELECT NOW()', (err, result) => {
-//     release()
-//     if (err) {
-//       return console.error('Error executing query', err.stack)
-//     }
-//     console.log('result rows in database', result.rows)
-//   })
-// })
 
 module.exports = pool;
 module.exports = db;
