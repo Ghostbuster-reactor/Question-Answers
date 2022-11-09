2f35c7301cff38793fc602c843e56044b07db9de
Tessa Thornberry <tthornberryclass@gmail.com>
2022-11-09 12:03:15 -0800

    added loader codes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/', router); //for requests
-
+app.get("/loaderio-f313d3e6de0e0fa32e593d5fc6b70cb6", (req, res) =>
+res.send("loaderio-f313d3e6de0e0fa32e593d5fc6b70cb6"))
 
 // const path = require('path');
 // app.use(express.static(path.join(__dirname, '../client/dist'))); //for when client is cloned
